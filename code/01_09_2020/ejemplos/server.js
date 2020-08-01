05eb2ca39e1b81eb09dcfe6137e60648f3ae4963
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2020-08-01 13:45:29 -0500

    Paso 2: Paso de mensajes mediante GET y POST basico.

--- code/01_09_2020/ejemplos/server.js
+++ code/01_09_2020/ejemplos/server.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const router = express.Router()
 
 var app = express()
 
-app.use('/', function(req, res) {
-    res.send('Hola')
+app.use(router)
+
+router.get('/', function(req, res) {
+    res.send('Hola desde get')
+})
+
+router.post('/', function(req, res) {
+    res.send('Hola desde post')
 })
 
 app.listen(3000)
