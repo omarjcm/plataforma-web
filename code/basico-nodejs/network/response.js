a18b1cbf804ed2aa6fd9a61916658579800d755d
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2020-08-06 08:34:45 -0500

    PASO 5: Respuestas coherentes (I Parte)

---
+++ code/basico-nodejs/network/response.js
@@ -0,0 +1,7 @@
+exports.success = function(req, res, message) {
+    res.send( message )
+}
+
+exports.response = function(req, res) {
+
+}
--- code/basico-nodejs/server.js
+++ code/basico-nodejs/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const response = require('./network/response')
+
 const router = express.Router()
 
 var app = express()
@@ -12,13 +14,13 @@ router.get('/message', function(req, res) {
     res.header({
         "custom-header":"Nuestro valor personalizado",
     })
-    res.status(201).send({error:'', body:'Creado correctamente.'})
+    response.success(req, res, 'Lista de mensajes.')
 })
 
-router.delete('/message', function(req, res) {
+router.post('/message', function(req, res) {
     console.log( req.query )
     console.log( req.body )
-    res.send('Mensaje eliminado correctamente. ' + req.body.text)
+    response.success(req, res, 'Creado correctamente.')
 })
 
 app.listen(3000)
