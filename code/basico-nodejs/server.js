e4003f50f382e196f242c1b7bff100abf4696c22
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2020-08-06 09:10:38 -0500

    PASO 7: Manejo de errores a nivel de desarrollador.

--- code/basico-nodejs/server.js
+++ code/basico-nodejs/server.js
@@ -20,7 +20,7 @@ router.get('/message', function(req, res) {
 router.post('/message', function(req, res) {
     console.log( req.query )
     if (req.query.error == 'ok') {
-        response.error(req, res, 'Error simulado.')
+        response.error(req, res, 'Error simulado.', 500, 'Es solo una simulacion de los errores.')
     } else {
         response.success(req, res, 'Creado correctamente.', 201)
     }
