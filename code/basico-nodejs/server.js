e1f30d3069d27f44f04460617d96a35192f13296
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2020-08-06 09:04:12 -0500

    PASO 6: Servir archivos estáticos (HTML, CSS, Javascript)

--- code/basico-nodejs/server.js
+++ code/basico-nodejs/server.js
@@ -26,5 +26,7 @@ router.post('/message', function(req, res) {
     }
 })
 
+app.use('/app', express.static('public'))
+
 app.listen(3000)
 console.log('La aplicación está escuchando en http://localhost:3000')
