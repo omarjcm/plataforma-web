3dffbd5cb8ab490956b149ed1633fa776fa7722b
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2020-08-07 01:51:06 -0500

    PASO 4: Añadiendo persistencia (mongodb).

--- code/proyecto-nodejs/components/message/store.js
+++ code/proyecto-nodejs/components/message/store.js
@@ -1,11 +1,29 @@
+const db = require('mongoose')
+const Model = require('./model')
+
+const uri = 'mongodb+srv://ups:clave@cluster0.pjyad.gcp.mongodb.net/ups?retryWrites=true&w=majority'
+
+db.Promise = global.Promise
+db.connect(uri, { 
+    useNewUrlParser:true, 
+    useUnifiedTopology:true,
+    dbName: 'ups'
+ })
+    .then(() => console.log('[db] Conectada con éxito.'))
+    .catch(error => console.error('[db] ', error))
+
 const list = []
 
 function addMessage( message ) {
-    list.push( message )
+    const myMessage = new Model(message)
+    myMessage.save()
+//    list.push( message )
 }
-    
-function getMessages() {
-    return list
+
+async function getMessages() {
+    //return list
+    const messages = await Model.find()
+    return messages
 }
 
 module.exports = {
---
+++ code/proyecto-nodejs/readme.txt
@@ -0,0 +1,11 @@
+Para poder ejecutar los ejemplos, se debe realizar lo siguiente:
+
+# Iniciar el proyecto con node.js
+npm init
+# Instalar express
+npm i express
+# Instalar body-parser
+npm i body-parser
+# Instalar nodemon para que se ejecute constamente (opcional)
+npm i nodemon
+npm i mongoose
