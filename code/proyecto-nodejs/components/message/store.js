7b07f00baa3761a7f21174ab7ecd7c24afc17f74
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2020-08-13 17:23:48 -0500

    PASO 11: Obtener la data relacionada.

--- code/proyecto-nodejs/components/message/store.js
+++ code/proyecto-nodejs/components/message/store.js
@@ -5,13 +5,22 @@ function addMessage( message ) {
     myMessage.save()
 }
 
-async function getMessages( filterUser ) {
-    let filter = {}
-    if (filterUser != null) {
-        filter = { user: filterUser }
-    }
-    const messages = await Model.find( filter )
-    return messages
+function getMessages( filterUser ) {
+    return new Promise((resolve, reject) => {
+        let filter = {}
+        if (filterUser != null) {
+            filter = { user: filterUser }
+        }
+        Model.find( filter )
+            .populate('user')
+            .exec((error, populated) => {
+                if (error) {
+                    reject( error )
+                    return false
+                }
+                resolve( populated )
+            })
+    })
 }
 
 async function updateMessage(id, message) {
